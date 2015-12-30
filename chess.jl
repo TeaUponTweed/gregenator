fcf6cb7e7e82724a8576ee99516781834f2fa92d
Michael Mason <TeaUponTweed@gmail.com>
2015-12-30 01:55:49 -0700

    Start using ncurses for display

--- chess.jl
+++ chess.jl
@@ -1,6 +1,7 @@
 abstract Piece # apparently bad practice see enum in http://docs.julialang.org/en/release-0.4/manual/style-guide/
 
 immutable Empty <: Piece
+  side::Bool
 end
 
 type Pawn <: Piece
@@ -30,6 +31,7 @@ end
 
 
 Pawn(x) = Pawn(false, x)
+Empty() = Empty(false)
 
 function chess_moves(side, board)
   N = sqrt(length(board))
@@ -61,67 +63,113 @@ function isValidMove(index, N)
 end
 
 function display(piece::Empty)
-  print(". ")
+  return ' '
 end
 
 function display(piece::Pawn)
-  print("p ")
+  return 'p'
 end
 
 function display(piece::Knight)
-  print("k ")
+  return 'n'
 end
 
 function display(piece::Bishop)
-  print("b ")
+  return 'b'
 end
 
 function display(piece::Rook)
-  print("r ")
+  return 'r'
 end
 
 function display(piece::Queen)
-  print("Q ")
+  return 'Q'
 end
 
 function display(piece::King)
-  print("K ")
+  return 'K'
 end
 
 function makeBoard()
-  transpose([
-    Rook(true)  Bishop(true)  Knight(true)  King(true)  Queen(true)  Knight(true)  Bishop(true)  Rook(true);
+  ([
+    Rook(true)  Knight(true)  Bishop(true)  King(true)  Queen(true)  Bishop(true)  Knight(true)  Rook(true);
     Pawn(true)  Pawn(true)    Pawn(true)    Pawn(true)  Pawn(true)   Pawn(true)    Pawn(true)    Pawn(true);
     Empty()     Empty()       Empty()       Empty()     Empty()      Empty()       Empty()       Empty();
     Empty()     Empty()       Empty()       Empty()     Empty()      Empty()       Empty()       Empty();
     Empty()     Empty()       Empty()       Empty()     Empty()      Empty()       Empty()       Empty();
     Empty()     Empty()       Empty()       Empty()     Empty()      Empty()       Empty()       Empty();
     Pawn(false) Pawn(false)   Pawn(false)   Pawn(false) Pawn(false)  Pawn(false)   Pawn(false)   Pawn(false);
-    Rook(false) Bishop(false) Knight(false) King(false) Queen(false) Knight(false) Bishop(false) Rook(false);
+    Rook(false) Knight(false) Bishop(false) King(false) Queen(false) Bishop(false) Knight(false) Rook(false);
   ])
 end
 
-function drawBoard(board)
-  N = sqrt(length(board))
-  println(N)
-  for (index, piece) in enumerate(board)
-    display(piece)
-    if index % N == 0
-      println()
+let
+  stdscr = ccall( (:initscr, :libncurses), Ptr{Void}, ())
+  ccall( (:cbreak, :libncurses), Int32, ())
+  ccall( (:noecho, :libncurses), Int32, ())
+  ccall( (:keypad, :libncurses), Int32, (Ptr{Void}, Bool), stdscr, true)
+  ccall( (:start_color, :libncurses), Int32, ())
+  # ccall((:resizeterm, :libncurses), Int32, (Int32, Int32), 8*2, 8*2)  
+
+  ccall((:init_pair, :libncurses), Int32, (Int16, Int16, Int16), 1, 0, 3)
+  ccall((:init_pair, :libncurses), Int32, (Int16, Int16, Int16), 2, 0, 11)
+  ccall((:init_pair, :libncurses), Int32, (Int16, Int16, Int16), 3, 15, 3)
+  ccall((:init_pair, :libncurses), Int32, (Int16, Int16, Int16), 4, 15, 11)
+
+  blackOnBlack = ccall((:COLOR_PAIR, :libncurses), Int32, (Int32, ), 1)
+  blackOnWhite = ccall((:COLOR_PAIR, :libncurses), Int32, (Int32, ), 2)
+  whiteOnBlack = ccall((:COLOR_PAIR, :libncurses), Int32, (Int32, ), 3)
+  whiteOnWhite = ccall((:COLOR_PAIR, :libncurses), Int32, (Int32, ), 4)
+
+  function drawSubsetWithColor(board, color_pair, criteria)
+    ccall((:attron, :libncurses), Int32, (Int32,), color_pair)
+    for row in 1:8
+      for col in 1:8
+        if criteria(board, row, col)
+          ccall((:wmove, :libncurses), Int32, (Ptr{Void}, Int32, Int32,), stdscr, row, col*2)
+          ccall((:waddch, :libncurses), Int32, (Ptr{Void}, UInt8,), stdscr, display(board[row, col]))
+          ccall((:wmove, :libncurses), Int32, (Ptr{Void}, Int32, Int32,), stdscr, row, col*2+1)
+          ccall((:waddch, :libncurses), Int32, (Ptr{Void}, UInt8,), stdscr, ' ')
+        end
+      end
     end
+    ccall((:attroff, :libncurses), Int32, (Int32,), color_pair)  
+  end
+
+  global drawChessBoard
+  function drawChessBoard(board)
+    ccall((:clear, :libncurses), Int32, ())
+    
+    drawSubsetWithColor(board, blackOnBlack, (b, r, c) -> (r % 2) != (c%2) && !b[r, c].side)
+    ccall((:refresh, :libncurses), Int32, ())
+    
+    drawSubsetWithColor(board, blackOnWhite, (b, r, c) -> (r % 2) == (c%2) && !b[r, c].side)
+    ccall((:refresh, :libncurses), Int32, ())
+    
+    drawSubsetWithColor(board, whiteOnBlack, (b, r, c) -> (r % 2) != (c%2) &&  b[r, c].side)
+    ccall((:refresh, :libncurses), Int32, ())
+    
+    drawSubsetWithColor(board, whiteOnWhite, (b, r, c) -> (r % 2) == (c%2) &&  b[r, c].side)
+    ccall((:wmove, :libncurses), Int32, (Ptr{Void}, Int32, Int32,), stdscr, 0, 0)
+    ccall((:refresh, :libncurses), Int32, ())
   end
 end
 
-drawBoard(makeBoard())
-println()
-tb = transpose([
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Knight(false) Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-  Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
-])
-drawBoard(tb)
+# println()
+# tb = transpose([
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Knight(false) Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+  # Empty() Empty() Empty() Empty() Empty() Empty() Empty() Empty();
+# ])
+
+try
+  drawChessBoard(makeBoard())
+  sleep(20)
+finally
+  ccall((:endwin, :libncurses), Int32, ())
+end
