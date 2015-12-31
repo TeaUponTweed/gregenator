61fe76d0e2490278a2d655e4a9017afe08ef7ade
Michael Mason <TeaUponTweed@gmail.com>
2015-12-31 13:36:44 -0700

    Add movement functions

--- chess.jl
+++ chess.jl
@@ -54,12 +54,132 @@ function XYToIndex(x, y, N)
   return N*y + x
 end
 
-function isValidMove(x, y, N)
+function movesInDir(board, x, y, dir)
+  dx, dy = dir
+  xx, yy = x + dx, y+dy
+  while inBounds(xx, yy)
+    if typeof(board[yy, xx]) == Empty
+      produce(xx, yy)
+    elseif board[yy, xx].side != board[y, x].side
+      produce(xx, yy)
+      break
+    else
+      break
+    end
+    xx += dx
+    yy += dy
+  end
+end
+
+function diagonalMoves(board, x, y)
+  for dir in [(i, j) for i in -1:2:1, j in -1:2:1]
+    movesInDir(board, x, y, dir)
+  end
+end
+
+function cardinalMoves(board, x, y)
+  for dir in [(1, 0) (-1, 0) (0, 1) (0, -1)]
+    movesInDir(board, x, y, dir)
+  end
+end
+
+function knightMoves(board, x, y)
+
+end
+
+function inBounds(x, y, N)
   return 0 < x <= N && 0 < y <= N
 end
 
-function isValidMove(index, N)
-  return 0 < index <= N
+inBounds(x, y) = inBounds(x, y, 8)
+
+function isValidMove(piece, otherPiece::Empty)
+  return true
+end
+
+function isValidMove(piece::Pawn, otherPiece)
+  return typeof(otherPiece) == Empty
+end
+
+function isValidMove(piece, otherPiece)
+  return piece.side != otherPiece.side
+end
+
+function isValidAttack(piece::Pawn, otherPiece::Empty)
+  return false
+end
+
+function isValidAttack(piece::Pawn, otherPiece)
+  return piece.side != otherPiece.side
+end
+
+function moves(piece::Knight, board, x, y)
+  for couple in [[(i, j) (j, i)] for i in -2:4:2, j in -1:2:1]
+    for move in couple
+      dx, dy = move
+      xx = x + dx
+      yy = y + dy
+      if inBounds(xx, yy) && isValidMove(piece, board[xx, yy])
+        produce(xx, yy)
+      end
+    end
+  end
+end
+
+function moves(piece, board, x, y)
+  return
+end
+
+function moves(piece::Bishop, board, x, y)
+  diagonalMoves(board, x, y)
+end
+
+function moves(piece::Rook, board, x, y)
+  cardinalMoves(board, x, y)
+end
+
+function moves(piece::Queen, board, x, y)
+  diagonalMoves(board, x, y)
+  cardinalMoves(board, x, y)
+end
+
+function moves(piece::King, board, x, y)
+  for dir in [(i, j) for i in -1:1, j in -1:1]
+    dx, dy = dir
+    if dx == dy == 0
+      continue
+    end
+    xx = x + dx
+    yy = y + dy
+    if inBounds(xx, yy) && isValidMove(piece, board[xx, yy])
+      produce(xx, yy)
+    end
+  end
+end
+
+function moves(piece::Pawn, board, x, y)
+  for dir in piece.hasMoved ? [(1, 0)] : [(1, 0) (2, 0)]
+    dy, dx = dir
+    if !piece.side
+      dy *= -1
+    end
+    xx = x + dx
+    yy = y + dy
+    if inBounds(xx, yy) && isValidMove(piece)
+      produce(xx, yy)
+    end
+
+  for dir in [(1, 1) (1, -1)]
+    dy, dx = dir
+    if !piece.side
+      dy *= -1
+    end
+    xx = x + dx
+    yy = y + dy
+    if inBounds(xx, yy) && isValidAttack(piece)
+      produce(xx, yy)
+    end
+  end
 end
 
 function display(piece::Empty)
