11a55c9276a9588af78bbbaf63d9ddcaef1c243b
Michael Mason <TeaUponTweed@gmail.com>
2016-07-27 00:48:54 -0600

    Update tests

--- test_gregenator.py
+++ test_gregenator.py
@@ -3,40 +3,29 @@ import random
 import computer_player
 import functools
 
-def negamax(board, depth, board_eval):
+def negamax(board, depth, color):
         if depth == 0:
-            return board_eval(board)
+            return color * computer_player.eval_board(board)
 
         max_score = float('-inf')
 
         for move in board.pseudo_legal_moves:
             board.push(move)
-            score = -negamax(board, depth - 1, board_eval)
+            score = -negamax(board, depth - 1, -color)
             board.pop()
             if score > max_score:
                 max_score = score
         return max_score
 
-def negamaxalphabeta(board, depth, alpha, beta, board_eval):
-        if depth == 0:
-            return board_eval(board)
-
-        for move in board.pseudo_legal_moves:
-            board.push(move)
-            score = -negamaxalphabeta(board, depth-1, -beta, -alpha, board_eval)
-            board.pop()
-            if score >= beta:
-                return beta
-            if score > alpha:
-                alpha = score
-        return alpha
 if __name__ == '__main__':
     board = chess.Board()
     while not board.is_game_over():
         for depth in [1, 2, 3]:
-            board_eval = functools.partial(computer_player.eval_board, color=board.turn)
-            assert (negamax(board, depth, board_eval) ==
-                    negamaxalphabeta(board, depth, float('-inf'), float('inf'), board_eval))
-            
+            color =  1 if board.turn else -1
+            ab =  computer_player.negamaxalphabeta(board, depth, float('-inf'), float('inf'), color)
+            assert (negamax(board, depth, color) == ab)
+            print(ab)
+            print(-computer_player.negamaxalphabeta(board, depth, float('inf'), float('-inf'), -color))
+            print('#'*8)
         board.push(random.choice(list(board.legal_moves)))
         print(board)
